4380c25387216be599678c038c4bf7041bf8ea91
jleluyer <jeremy.le.luyer@gmail.com>
2017-07-27 08:56:30 -1000

    Update 07_cpg_prediction.sh

--- 00_scripts/07_cpg_prediction.sh
+++ 00_scripts/07_cpg_prediction.sh
@@ -22,7 +22,7 @@ OUTPUTTJ="TJ.CpGi"
 cpgplot -sequence "$GENOME" -window 100 -minlen 500 -minoe 0.65 -minpc 55.0 -outfile "$OUTPOUT_FOLDER"/"$OUTPUTTJ".cpgplot -graph none -outfeat "$OUTPOUT_FOLDER"/"$OUTPUTTJ".gff
 
 # Gardiner-Garden approach (Gardiner-Garden et al., 1987)
-cpgplot -sequence "$GENOME" -window 100 -minlen 200 -minoe 0.6 -minpc 50.0 -outfile "$OUTPOUT_FOLDER"/"$OUTPUTGGM".cpgplot -graph none -outfeat "$OUTPOUT_FOLDER"/"$OUTPUTGGM".gff
+cpgplot -sequence "$GENOME" -window 100 -minlen 200 -minoe 0.6 -minpc 50.0 -outfile "$OUTPOUT_FOLDER"/"$OUTPUTGG".cpgplot -graph none -outfeat "$OUTPOUT_FOLDER"/"$OUTPUTGG".gff
 
 #Gardiner-Garden masked approach (Bock et al., 2007)
 cpgplot -sequence "$GENOMEMSK" -window 100 -minlen 200 -minoe 0.6 -minpc 50.0 -outfile "$OUTPOUT_FOLDER"/"$OUTPUTGGM".cpgplot -graph none -outfeat "$OUTPOUT_FOLDER"/"$OUTPUTGGM".gff
